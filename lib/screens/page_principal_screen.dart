544bff3d77efd487086befc2d26cc3977b3e2d76
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2021-12-02 19:25:38 -0300

    RefreshBuilder

--- lib/screens/page_principal_screen.dart
+++ lib/screens/page_principal_screen.dart
@@ -1,14 +1,13 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:plantao_farma/app_routes.dart';
 import 'package:plantao_farma/components/app_bar_c.dart';
 import 'package:plantao_farma/components/farmacia_grid.dart';
 import 'package:plantao_farma/components/navigator_bar.dart';
 import 'package:plantao_farma/models/farmacia.dart';
 import 'package:plantao_farma/provides/services/firestore_service.dart';
-import 'package:plantao_farma/utils/app_color.dart';
 import 'package:provider/provider.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 class PagePrincipalScreen extends StatefulWidget {
   const PagePrincipalScreen({ Key? key }) : super(key: key);
@@ -22,29 +21,35 @@ class _PagePrincipalScreenState extends State<PagePrincipalScreen> {
   Widget build(BuildContext context) {
   FirestoreService firestore = Provider.of<FirestoreService>(context);
    final List<Farmacia> farmacia = firestore.list;
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              AppBarC(),
-              SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height-284,
-                child: ListView.builder(
-                  itemCount: farmacia.length,
-                  itemBuilder: (_, index){
-                    return FarmaciaGrid(farmacia: farmacia[index]);
-                  }
+    return TimerBuilder.periodic(
+      Duration(minutes: 1),
+      builder: (context){
+      return SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                AppBarC(),
+                SizedBox(
+                  height: 15,
                 ),
-              ),
-              NavigatorBar(),
-            ],
-          ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height-284,
+                  child: ListView.builder(
+                    itemCount: farmacia.length,
+                    itemBuilder: (_, index){
+                      return FarmaciaGrid(farmacia: farmacia[index]);
+                    }
+                  ),
+                ),
+                NavigatorBar(),
+              ],
+            ),
+          )
         )
-      )
+      );
+      },
     );
   }
+  
 }
